3844187e66b21750f24755bc6ab86a8b9306146b
Robert Engel <robert.engel@apollidon.com>
2021-06-21 17:26:35 -0400

    cleaning up and adding styles to post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,9 @@
 import Layout from '../../components/layout';
+import Head from 'next/head';
 
 import { getAllPostIds, getPostData } from '../../lib/posts';
-import Head from 'next/head';
+import Date from "../../components/date";
+import utilStyles from '../../styles/utils.module.css'
 
 export async function getStaticProps({ params }) {
     const postData = await getPostData(params.id)
@@ -26,13 +28,13 @@ export default function Post({ postData }) {
             <Head>
                 <title>{ postData.title }</title>
             </Head>
-            { postData.title }
-            <br />
-            { postData.id }
-            <br />
-            { postData.date }
-            <br />
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            <article>
+                <h1 className={ utilStyles.headingXl }>{ postData.title }</h1>
+                <div className={ utilStyles.lightText }>
+                    <Date dateString={ postData.date } />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
     )
 }
