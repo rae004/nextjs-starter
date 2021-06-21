92eb72b64fe404b2eae4b1b3a6995b25e498a1e1
Robert Engel <robert.engel@apollidon.com>
2021-06-21 15:25:21 -0400

    format update

--- pages/index.js
+++ pages/index.js
@@ -36,15 +36,15 @@ export default function Home({ allPostsData }) {
             <section className={`${ utilStyles.headingMd } ${ utilStyles.padding1px }`}>
                 <h2 className={ utilStyles.headingLg }>Blog</h2>
                 <ul className={ utilStyles.list }>
-                    {allPostsData.map(({ id, date, title }) => (
-                        <li className={utilStyles.listItem} key={id}>
+                    { allPostsData.map(({ id, date, title }) => (
+                        <li className={ utilStyles.listItem } key={ id }>
                             { title }
                             <br />
                             { id }
                             <br />
                             { date }
                         </li>
-                    ))}
+                    )) }
                 </ul>
             </section>
         </Layout>
