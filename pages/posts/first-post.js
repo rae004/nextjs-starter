7945d5bb72c1265b0d606725acc87c8bba169493
Robert Engel <robert.engel@apollidon.com>
2021-06-21 12:48:45 -0400

    updating first post page. adding app.js file, images, and styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,20 @@
 import Link from 'next/link'
+import Head from 'next/head'
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
     return (
-        <section>
-            <h1>First Post</h1>
-            <dl>
-                <dt>Next JS tutorial</dt>
-                <dd><Link href={'/'}>Back to Home</Link></dd>
-            </dl>
-        </section>
+        <Layout>
+            <Head>
+                <title>RAE-DEV First Page</title>
+            </Head>
+            <section>
+                <h1>First Post</h1>
+                <dl>
+                    <dt>Next JS tutorial</dt>
+                    <dd><Link href={'/'}>Back to Home</Link></dd>
+                </dl>
+            </section>
+        </Layout>
     )
 }
